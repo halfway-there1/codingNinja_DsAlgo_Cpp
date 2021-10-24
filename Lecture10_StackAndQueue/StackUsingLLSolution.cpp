628d80ed02ac74e80b360afd56787b96dea97989
Anurag Kumar <anuragkushwaahan@gmail.com>
2021-10-24 21:14:38 +0530

    added CN's and my solution for stackUsingLL

---
+++ Lecture10_StackAndQueue/StackUsingLLSolution.cpp
@@ -0,0 +1,96 @@
+#include <iostream>
+using namespace std;
+
+class Node {
+   public:
+    int data;
+    Node *next;
+
+    Node(int data) {
+        this->data = data;
+        next = NULL;
+    }
+};
+
+class Stack {
+	// Define the data members
+    Node *head;
+    int size;
+    public:
+    Stack() {
+        // Implement the Constructor
+        head = nullptr;
+        size = 0;
+    }
+
+	/*----------------- Public Functions of Stack -----------------*/
+
+    int getSize() {
+        // Implement the getSize() function
+        return size;
+    }
+
+    bool isEmpty() {
+        // Implement the isEmpty() function
+        return size == 0;
+    }
+
+    void push(int element) {
+        // Implement the push() function
+        Node *newNode = new Node(element);
+        newNode->next = head;
+        head = newNode;
+        size++;
+    }
+
+    int pop() {
+        // Implement the pop() function
+        if(head == nullptr){
+            return -1;
+        }
+        Node *toDelete = head;
+        head = head->next;
+        int popped = toDelete->data;
+        delete toDelete;
+        size--;
+        return popped;
+    }
+
+    int top() {
+        // Implement the top() function
+        if(size == 0){
+            return -1;
+        }
+        return head->data;
+    }
+};
+
+int main() {
+    Stack st;
+
+    int q;
+    cin >> q;
+
+    while (q--) {
+        int choice, input;
+        cin >> choice;
+        switch (choice) {
+            case 1:
+                cin >> input;
+                st.push(input);
+                break;
+            case 2:
+                cout << st.pop() << "\n";
+                break;
+            case 3:
+                cout << st.top() << "\n";
+                break;
+            case 4:
+                cout << st.getSize() << "\n";
+                break;
+            default:
+                cout << ((st.isEmpty()) ? "true\n" : "false\n");
+                break;
+        }
+    }
+}
