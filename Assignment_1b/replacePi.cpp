582f029cdee4e05f9999c60e54a98ab6b5731392
halfway-there1 <90105000+halfway-there1@users.noreply.github.com>
2024-03-15 19:40:01 +0530

    improve formatting

--- Assignment_1b/replacePi.cpp
+++ Assignment_1b/replacePi.cpp
@@ -1,31 +1,32 @@
 // Change in the given string itself. So no need to return or print anything
 #include <iostream>
+#include <cstring>
 using namespace std;
 
-void replacePi(char* input, int size){
-    if(size <= 1){
+void replacePi(char *input, int size) {
+    if (size <= 1) {
         return;
     }
-    if(input[0] == 'p' && input[1] == 'i'){
-        for(int i = size; i > 1; i--){
-            input[i+2] = input[i];
+    
+    if (input[0] == 'p' && input[1] == 'i') {
+        for (int i = size; i > 1; i--) {
+            input[i + 2] = input[i];
         }
         input[0] = '3';
         input[1] = '.';
         input[2] = '1';
         input[3] = '4';
-        size = size+2;  // size is increased by 2 if 'pi' is replaced by 3.14
-        replacePi(input+4,size-4);
+        size = size + 2; // size is increased by 2 if 'pi' is replaced by 3.14
+        replacePi(input + 4, size - 4);
         return;
     }
-    replacePi(input + 1, size -1);
+    
+    replacePi(input + 1, size - 1);
 }
 
 void replacePi(char input[]) {
-	// Write your code here
-    int l;
-    for(l = 0; input[l] != '\0'; l++);
-
+    // Write your code here
+    int l = strlen(input);
     replacePi(input, l);
 }
 
@@ -35,4 +36,3 @@ int main() {
     replacePi(input);
     cout << input << endl;
 }
-
