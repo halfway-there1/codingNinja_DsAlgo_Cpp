1dadab845d024f1ffe3bb532f0d136e694f9613e
halfway-there1 <90105000+halfway-there1@users.noreply.github.com>
2024-03-24 01:18:19 +0530

    Refactor mergeSort function and add merge_sorted_segments function

--- Lecture_4_Recursion_2/mergeSort_vector.cpp
+++ Lecture_4_Recursion_2/mergeSort_vector.cpp
@@ -4,35 +4,54 @@ using namespace std;
 #include <anurag_defines.h>
 #include <debugging.h>
 
-void mergeSort(vector<int> &v, int si, int ei) {
+void merge_sorted_segments(vi &v, int si, int mid, int ei) {
+    vi lhs(v.begin() + si, v.begin() + mid + 1);
+    vi rhs(v.begin() + mid + 1, v.begin() + ei + 1);
+
+    int i = 0, n1 = lhs.size();
+    int j = 0, n2 = rhs.size();
+    int k = si;
+
+    while (i < n1 && j < n2) {
+        if (lhs[i] <= rhs[j])
+            v[k++] = lhs[i++];
+        else
+            v[k++] = rhs[j++];
+    }
+
+    while (i < n1) {
+        v[k++] = lhs[i++];
+    }
+
+    while (j < n2) {
+        v[k++] = rhs[j++];
+    }
+}
+
+void merge_sort(vi &v, int si, int ei) {
     if (si >= ei)
         return;
 
-    int m = si + (ei - si) / 2;
-    mergeSort(v, si, m);
-    mergeSort(v, m + 1, ei);
+    int mid = si + (ei - si) / 2;
+    merge_sort(v, si, mid);
+    merge_sort(v, mid + 1, ei);
 
-    // lhs = v[si...m]
-    vector<int> lhs(v.begin() + si, v.begin() + m + 1);
-    // rhs = v[m+1...ei]
-    vector<int> rhs(v.begin() + m + 1, v.begin() + ei + 1);
-
-    merge(all(lhs), all(rhs), v.begin() + si);
+    // the segments being v[si...mid] and v[mid+1...ei]
+    merge_sorted_segments(v, si, mid, ei);
 }
 
 void solve() {
-    int m;
-    cin >> m;
+    int n;
+    cin >> n;
+    vi v(n);
+    for (int &num : v)
+        cin >> num;
 
-    vector<int> a(m);
-    for (auto &ai : a)
-        cin >> ai;
+    debug(v);
+    merge_sort(v, 0, n - 1);
 
-    mergeSort(a, 0, m - 1);
-
-    for (auto ai : a)
-        cout << ai << " ";
-    cout << endl;
+    for (auto num : v)
+        cout << num << " ";
 }
 
 int32_t main() {
