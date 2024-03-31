2db7b8931015ccb3104e9f41760012efdae8986a
halfway-there1 <90105000+halfway-there1@users.noreply.github.com>
2024-04-01 00:58:48 +0530

    Refactor quickSort.cpp: Improve code readability and use standard library functions

--- Lecture_4_Recursion_2/quickSort.cpp
+++ Lecture_4_Recursion_2/quickSort.cpp
@@ -1,71 +1,72 @@
-#include<iostream>
+#include <algorithm> // for swap() fxn
+#include <iostream>
 using namespace std;
 
-int partition(int input [],int si,int ei){
+int partition(int input[], int si, int ei) {
     int pivot = input[si];
     int actual_pivotIndex = si;
 
-    for(int i = si+1; i <= ei; i++ ){
-        if(input[i] <= pivot){
+    // placing the pivot at its right index.
+    for (int i = si + 1; i <= ei; i++) {
+        if (input[i] <= pivot) {
             actual_pivotIndex++;
         }
     }
-    input[si] = input[actual_pivotIndex];
-    input[actual_pivotIndex] = pivot;
+    swap(input[si], input[actual_pivotIndex]);
 
-    for(int i = si, j = ei; i < actual_pivotIndex && j > actual_pivotIndex;){
-        if(!(input[i] <= pivot)){
-            if(!(input[j] > pivot)){
-                int temp = input[i];
-                input[i] = input[j];
-                input[j] = temp;
+    // partitioning process.
+    for (int i = si, j = ei; i < actual_pivotIndex && j > actual_pivotIndex;) {
+        if (!(input[i] <= pivot)) {
+            if (!(input[j] > pivot)) {
+                swap(input[i], input[j]);
                 i++;
                 j--;
-            }
-            else{
+            } else {
                 j--;
             }
-        }
-        else{
+        } else {
             i++;
-            if(input[j] > pivot){
+            if (input[j] > pivot) {
                 j--;
             }
         }
     }
-    
+
     return actual_pivotIndex;
 }
 
-void quickSort(int input[], int si, int ei){
-    if(si >= ei){
+void quickSort(int input[], int si, int ei) {
+    if (si >= ei) {
         return;
     }
-    int actual_pivotIndex = partition(input,si,ei);
-    quickSort(input,si,actual_pivotIndex-1);
-    quickSort(input,actual_pivotIndex+1,ei);
+
+    int actual_pivotIndex = partition(input, si, ei);
+    quickSort(input, si, actual_pivotIndex - 1);
+    quickSort(input, actual_pivotIndex + 1, ei);
+
     return;
 }
 
-void quickSort(int input[], int size){
+void quickSort(int input[], int size) {
     quickSort(input, 0, size - 1);
 }
 
-int main(){
+int main() {
     int n;
     cin >> n;
-  
+
     int *input = new int[n];
-    
-    for(int i = 0; i < n; i++) {
+
+    for (int i = 0; i < n; i++) {
         cin >> input[i];
     }
-    
-    quickSort(input,n);
-    for(int i = 0; i < n; i++) {
+
+    quickSort(input, n);
+    for (int i = 0; i < n; i++) {
         cout << input[i] << " ";
     }
-    
-    delete [] input;
 
+    delete[] input;
 }
+
+// TCA
